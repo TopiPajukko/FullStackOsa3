7f920076698e025f3fb685f414bccbcaf00b277c
TopiPajukko <topi.pajukko@dnainternet.net>
2023-10-18 11:59:47 +0300

    teht 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 let persons = 
     {
