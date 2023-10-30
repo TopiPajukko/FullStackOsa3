c2dd39979080e29c96cbb9f6b7a1853af31d8f76
TopiPajukko <topi.pajukko@dnainternet.net>
2023-10-30 12:36:54 +0200

    3 osa kaikki tehtävät

--- index.js
+++ index.js
@@ -10,64 +10,38 @@ app.use(express.json())
 app.use(morgan('tiny'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-let persons = 
-    {
-        /* "persons": [
-          {
-            "name": "Ada Lovelace",
-            "number": "123",
-            "id": 1
-          },
-          {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 2
-          },
-          {
-            "name": "Ada Lovelacerz",
-            "number": "12345",
-            "id": 3
-          },
-          {
-            "name": "Dan Abramovich",
-            "number": "12-43-23434125",
-            "id": 4
-          } 
-        ] */
-      }
-
 app.get('/', (req, res) => {
-  res.send()
+    res.send()
 })
 
 app.get('/api/persons', (req, res, next) => {
-  Personnel.find({})
-  .then(result => {
-    console.log(result)
-    res.json(result)
-  }) .catch(error => next(error))
+    Personnel.find({})
+        .then(result => {
+            console.log(result)
+            res.json(result)
+        }) .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
 
-	Personnel
-		.findById(req.params.id)
-		.then(person => {
-			if(person){
-				res.json(person)
-			}else{
-				res.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+    Personnel
+        .findById(req.params.id)
+        .then(person => {
+            if(person){
+                res.json(person)
+            }else{
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-	Personnel.findByIdAndRemove(req.params.id)
-		.then(() => {
-			res.status(204).end()
-		})
-		.catch(error => next(error))
+    Personnel.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -77,46 +51,46 @@ app.post('/api/persons', postMorgan, (req, res, next) => {
 
     if(!body.name || !body.number) {
         return res.status(400).json({
-		error: 'name or number missing'
-		})
+            error: 'name or number missing'
+        })
     }else{
-    const person = new Personnel({
-    name: body.name,
-    number: body.number,
-    })
-    person.save()
-    .then(person => {res.json(person)
-    }).catch(error => next(error))
-  }
+        const person = new Personnel({
+            name: body.name,
+            number: body.number,
+        })
+        person.save()
+            .then(person => {res.json(person)
+            }).catch(error => next(error))
+    }
 })  
 
 app.get('/info', (req, res, next) => {
     const date = new Date()
     Personnel.find({})
-    .then(result => {
-	    res.send(
-        `<p>Phonebook has info for ${result.length} persons</p>
+        .then(result => {
+            res.send(
+                `<p>Phonebook has info for ${result.length} persons</p>
                <br>
                <p>${date}</p>`
-			)
-      }).catch(error => next(error))
+            )
+        }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-	const { name, number } = req.body
+    const { name, number } = req.body
 
-	Personnel.findByIdAndUpdate(
-		req.params.id,
-		{ name, number },
-		{ new: true, runValidators: true, context: 'query' }
-	)
-		.then(updatedPerson => {
-			res.json(updatedPerson)
-		})
-		.catch(error => next(error))
+    Personnel.findByIdAndUpdate(
+        req.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -21,24 +21,24 @@ const personnelSchema = new mongoose.Schema({
   
 const Personnel = mongoose.model('person', personnelSchema)
   
-  if(process.argv.length===3){
+if(process.argv.length===3){
     
     Personnel.find({}).then(result => {
-      console.log('phonebook: ')
-      result.forEach(personnel => {
-        console.log(`${personnel.name} ${personnel.number}`)
-      })
-      mongoose.connection.close()
+        console.log('phonebook: ')
+        result.forEach(personnel => {
+            console.log(`${personnel.name} ${personnel.number}`)
+        })
+        mongoose.connection.close()
     })
   
-  }else{
+}else{
     const personnel = new Personnel({
-      name: process.argv[3],
-      number: process.argv[4],
+        name: process.argv[3],
+        number: process.argv[4],
     }) 
     
     personnel.save().then(result => {
-      console.log(`added ${result.name} number ${result.number} to phonebook`)
-      mongoose.connection.close()
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
     })
-  }
+}
